c0be4af8d52102c85de11644fd80853ea7160081
R K Chitra <chitra31399@gmail.com>
2021-06-04 14:40:55 +0530

    Front End

--- webcam/main.py
+++ webcam/main.py
@@ -3,45 +3,55 @@ import jsonify
 import base64
 import serial
 import time
-def setuparduino():
-	arduino = serial.Serial(port='COM8', baudrate=9600, timeout=.1) 
-	print("Setting up arduino....")
-	time.sleep(4)
-	print("Arduino connected")
-	return arduino
-def left(arduino):
-	arduino.write(b'1')
-def start(arduino):
-	arduino.write(b'2')
-def right(arduino):
-	arduino.write(b'0')
-def movebiodegradabe():
-	ard = setuparduino()
-	right(ard)
-	time.sleep(5)
-	start(ard)
-	ard.close()
-def movenonbiodegradabe():
-	ard = setuparduino()
-	left(ard)
-	time.sleep(5)
-	start(ard)
-	ard.close()
+import string    
+import random
+import os.path
+from os import path
+#def setuparduino():
+#	arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1) 
+#	print("Setting up arduino....")
+#	time.sleep(4)
+#	print("Arduino connected")
+#	return arduino
+#def left(arduino):
+#	arduino.write(b'1')
+#def start(arduino):
+#	arduino.write(b'2')
+#def right(arduino):
+#	arduino.write(b'0')
+#def movebiodegradabe():
+#	ard = setuparduino()
+#	right(ard)
+#	time.sleep(5)
+#	start(ard)
+#	ard.close()
+#def movenonbiodegradabe():
+#	ard = setuparduino()
+#	left(ard)
+#	time.sleep(5)
+#	start(ard)
+#	ard.close()
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-	movenonbiodegradabe()
+	#movenonbiodegradabe()
 	return render_template('signup-2.html')
 @app.route('/handleimage',methods =['POST'])
 def handleimage():
     if request.method == 'POST':
         print(type(request.form['file']))
         print("here")
-        movebiodegradabe()
-        with open('sample.png', 'wb') as f:
+        #movebiodegradabe()
+        S = 10   
+        # call random.choices() string module to find the string in lowercase + numeric data.  
+        ran = ''.join(random.choices(string.ascii_lowercase+ string.digits, k = S))+'.png' 
+        while path.exists(ran) :
+            ran = ''.join(random.choices(string.ascii_lowercase+ string.digits, k = S))+'.png' 
+        with open(ran, 'wb') as f:
             f.write(base64.decodebytes(request.form['file'].split(',')[1].encode()))
-        return "Image url recieved , check console"
+            print("Image name: ",ran)
+        return "Image successfully saved"
     return "Get not allowed"
 if __name__ == "__main__":
     app.run(debug=True)
