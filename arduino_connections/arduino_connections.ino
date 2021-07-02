d5325ac88a8574a2c284f34642868a26bdd9b087
AnvitaMurthy <56925491+AnvitaMurthy@users.noreply.github.com>
2021-07-02 18:01:36 +0530

    Update arduino_connections.ino

--- arduino_connections/arduino_connections.ino
+++ arduino_connections/arduino_connections.ino
@@ -1,11 +1,12 @@
 #include <Servo.h>
 Servo myservo; 
-char myByteArray;
+char myByteArray;  
 int i;
 void setup() {
-  myservo.attach(3);
-  Serial.begin(9600);
-  myservo.write(60);
+  myservo.attach(3);    #attaching server motor to pin 3 of arduino
+    
+  Serial.begin(9600);   #opening a connection at the port 9600 so that python code can communicate with Arduino
+  myservo.write(60);    # setting up the arduino starting at the middle position
 }
 
 void loop()
@@ -13,22 +14,22 @@ void loop()
 
     while(Serial.available())
     {
-      myByteArray = Serial.read();
+      myByteArray = Serial.read(); #reading what python script wrote in the communicating serial connection
       if(myByteArray=='1')
       {
-        for(i=59; i!=20; i--)
+        for(i=59; i!=20; i--)   #tilting the platform left
         {
         myservo.write(i);
         delay(50);
         } 
       }
-      if(myByteArray=='2')
+      if(myByteArray=='2')  # bringing arduino to initial position
       {
         myservo.write(60);
       }
       if(myByteArray=='0')
       {
-        for(i=61; i!=100; i++)
+        for(i=61; i!=100; i++) #tilting right
         {
         myservo.write(i);
         delay(50);
